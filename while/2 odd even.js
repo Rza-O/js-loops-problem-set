4df8268614a540dd8d364a80870d1e1c398ef10c
appoclayptor <shahreza1414@gmail.com>
2024-09-02 19:37:47 +0600

    Solved two while loops problems

--- while/2 odd even.js
+++ while/2 odd even.js
@@ -16,3 +16,19 @@ Find all the even numbers from 78 to 98.
  */
 
 /*programming hero*/
+
+// Task-1
+let num = 61;
+while(num <= 100){
+    console.log('Odd number is', num);
+    num += 2;
+}
+
+// Task-2
+let num = 78;
+while(num <= 98){
+    if(num % 2 === 0){
+        console.log('Even number is:', num);
+    }
+    num++;
+}
