430ec624981d3745184827198ea5094ecda7ea83
appoclayptor <shahreza1414@gmail.com>
2024-09-04 21:01:29 +0600

    Added another solution

--- break/2 sum upto 100.js
+++ break/2 sum upto 100.js
@@ -1,3 +1,15 @@
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
+
+let x = 1;
+let sum = 0;
+
+while(true){
+    sum += x;
+    if(sum >= 100){
+        break;
+    }
+    x++;
+}
+console.log(sum)
