3b6bd1ccdb56048d30fd21b3edf7f14df23b3383
appoclayptor <shahreza1414@gmail.com>
2024-09-04 19:53:22 +0600

    Solved Multiplication table problem with for loop

--- for/4 multiplication table.js
+++ for/4 multiplication table.js
@@ -6,3 +6,8 @@ Generate a multiplication table for number 9
 
 
 /*programming hero*/
+// i took help from a forum
+
+for(let i = 1; i <= 10; i++){
+    console.log('9 multiplies by', i, '=', 9*i );
+}
